e9ec6b069138d1fd4d891ba703288b9fc3320f9d
zyunjie <zhangyj>
2020-05-14 14:44:58 +0800

    '让坦克根据方向键移动'

---
+++ src/main/java/com/tanke/demo/tanke1/Dir.java
@@ -0,0 +1,9 @@
+package com.tanke.demo.tanke1;
+
+/**
+ * created by zyj on 2020/5/14
+ * 方向变量
+ */
+public enum Dir {
+    LEFT,UP,RIGHT,DOWN
+}
--- src/main/java/com/tanke/demo/tanke1/TankeFrame.java
+++ src/main/java/com/tanke/demo/tanke1/TankeFrame.java
@@ -8,13 +8,16 @@ import java.awt.event.*;
  */
 public class TankeFrame extends Frame {
 
-    int x=200, y=200;
+    private  int x = 200, y = 200;
+    //每次移动速度
+    final  int SPEED=10;
+    Dir dir = Dir.LEFT;
 
-    public TankeFrame(){
+    public TankeFrame() {
         //frame就是一个窗口类
 //        Frame f = new Frame();
         setVisible(true);
-        setSize(800,600);
+        setSize(800, 600);
         setResizable(false);
         setTitle("tanke game");
         addWindowListener(new WindowAdapter() {
@@ -31,22 +34,95 @@ public class TankeFrame extends Frame {
 
     //窗口重新绘制的时候自动执行这个方法
     @Override
-    public void paint(Graphics graphics){
-        graphics.fillRect(x,y,50,50);
-
-        y +=10;
+    public void paint(Graphics graphics) {
+        graphics.fillRect(x, y, 50, 50);
+        switch (dir) {
+            case LEFT:
+                x-=SPEED;
+                break;
+            case UP:
+                y-=SPEED;
+                break;
+            case RIGHT:
+                x+=SPEED;
+                break;
+            case DOWN:
+                y+=SPEED;
+                break;
+            default:
+                break;
+        }
+//        y +=10;
     }
 
     class MyKeyListener extends KeyAdapter {
+        //通过判断方向键是否为true确认往那个方向移动 或者斜向
+        boolean bl = false;
+        boolean br = false;
+        boolean bu = false;
+        boolean bd = false;
+
         @Override
-        public void keyPressed(KeyEvent event){
+        public void keyPressed(KeyEvent event) {
+            //获取键代码
+            int key = event.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+//                    x-=10;
+                    bl = true;
+                    break;
+                case KeyEvent.VK_UP:
+//                    y-=10;
+                    bu = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+//                    x+=10;
+                    br = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+//                    y+=10;
+                    bd = true;
+                    break;
+                default:
+                    break;
+            }
 //            x +=10;
 //            repaint();
-            System.out.println("key pressed");
+            setTankeDir();
         }
+
+
         @Override
-        public void keyReleased(KeyEvent event){
-            System.out.println("key released");
+        public void keyReleased(KeyEvent event) {
+            int key = event.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+//                    x-=10;
+                    bl = false;
+                    break;
+                case KeyEvent.VK_UP:
+//                    y-=10;
+                    bu = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+//                    x+=10;
+                    br = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+//                    y+=10;
+                    bd = false;
+                    break;
+                default:
+                    break;
+            }
+            setTankeDir();
+        }
+
+        private void setTankeDir() {
+            if (bl) dir = Dir.LEFT;
+            if (bu) dir = Dir.UP;
+            if (br) dir = Dir.RIGHT;
+            if (bd) dir = Dir.DOWN;
         }
     }
 
